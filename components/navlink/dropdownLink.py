0575351cc9d4a1b99183b59e2539d571f867be04
cliff0917 <tcfsh210119@gmail.com>
2023-01-26 08:01:02 +0800

    fix style of dropdown nav-link

--- components/navlink/dropdownLink.py
+++ components/navlink/dropdownLink.py
@@ -1,7 +1,13 @@
 import dash_bootstrap_components as dbc
 
 def serve(text, link):
-    return dbc.DropdownMenuItem(
-        text, external_link=True, href=link, 
+    return dbc.NavItem(
+        dbc.DropdownMenuItem(
+            text, external_link=True, href=link, 
+            id={
+                'type': 'nav-link',
+                'index': link
+            },
+        ),
         style={'textAlign': 'center', 'fontSize': 18}
     )
