6f79fc37917aa78ca0b246ab5dd0d3e46c3bf0cc
cliff0917 <tcfsh210119@gmail.com>
2023-03-24 12:27:38 +0800

    Remove home intro files

--- components/services/paragraph.py
+++ components/services/paragraph.py
@@ -1,9 +1,18 @@
 import feffery_antd_components as fac
 from dash import html
 
+import globals
+
 def serve(types):
-    with open(f"./assets/{types}/text/home_intro.txt", 'r') as f:
-        lines = f.readlines()
+    type_chinese = globals.config['chinese'][types]['normal']
+    predict_text = globals.config['chinese'][types]['predict_text']
+    type_text = type_chinese + predict_text
+
+    lines = [
+        f'運用 AI 評估{type_text}，',
+        f'取得{type_text}結果圖，',
+        f'以及{type_chinese}照護衛教資訊。'
+    ]
 
     paragraph = html.Div(
         [
